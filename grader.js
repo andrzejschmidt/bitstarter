3dcc5d532a020d7527eb89e657ae20e48a0299b2
andrzejschmidt <andrzej.schmidt@gmail.com>
2013-07-25 02:32:42 +0100

    Update grader.js

--- grader.js
+++ grader.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
+
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
 
 References:
-
 + cheerio
 - https://github.com/MatthewMueller/cheerio
 - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
@@ -24,95 +24,88 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
-var util = require('util');
+var restler = require('restler');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
+    if (!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1); // http://nodejs.org/api/process.html (search for process exit code)
     }
     return instr;
 };
 
-var assertURLCorrect = function(inurl) {
-    var instr = inurl.toString();
-    var url = require('url');
-    var urlObject = url.parse(inurl);
-    if (urlObject['protocol']!='http:') {
-        console.log("%s is not a correct url. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
+var cheerioHtmlFile = function(htmlfile){
+    return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var checkURLresponse = function(checksFile, toConsole) {
-    var response2url = function(result, response) {
-        if (result instanceof Error) {
-            console.error('Error: ' + util.format(response.message));
-        } else {
-var checkJson = checkHtml(response.rawEncoded, checksFile);
-if (toConsole) console.log(JSON.stringify(checkJson, null, 4));
-        }
-return checkJson;
-    };
-    return response2url;
+var loadChecks = function(checksFile) {
+    return JSON.parse(fs.readFileSync(checksFile));
 };
 
-var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
-};
-
-var checkHtml = function(html, checksFile) {
-    $ = cheerio.load(html);
-    var checks = loadChecks(checksFile).sort();
+var checkHtmlFile = function(htmlfile, checksfile) {
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
     var out = {};
-    for(var ii in checks) {
+    for (var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
 };
 
-var checkHtmlFile = function(htmlurl, checksFile, toConsole) {
-    var url = require('url');
-    var urlObject = url.parse(htmlurl);
-    if (urlObject['protocol']=='http:') {
-var response = checkURLresponse(checksFile, toConsole);
-rest.get(htmlurl).on('complete', response);
-    } else {
-var html = fs.readFileSync(htmlurl);
-var checkJson = checkHtml(html, checksFile);
-if (toConsole) console.log(JSON.stringify(checkJson, null, 4));
-    }
-    return checkJson;
+// This function initializes and returns a function that can be used
+// by the restler .on event. I chose to add a callback function since this
+// function is asynchronous. Evidently checkHtmlFile and checkHtmlUrl behave differently
+// which can be bad if we use grader.js mostly as a module instead of standalone.
+var checkHtmlUrl = function(checksfile, callback) {
+    var response = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            $ = cheerio.load(result);
+            var checks = loadChecks(checksfile).sort();
+            var out = {};
+            for (var ii in checks) {
+                var present = $(checks[ii]).length > 0;
+                out[checks[ii]] = present;
+            }
+            callback(out);
+        }
+    };
+    return response;
 };
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
+    // Workaround for commander.js issue. <http://stackoverflow.com/a/6772648>
     return fn.bind({});
 };
 
-if (require.main == module) {
+if ( require.main == module ) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists))
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
-        .option('-u, --url <url>', 'URL to html file', clone(assertURLCorrect))
-        .parse(process.argv);
-    if (program.checks === undefined) {
-        console.log("Must specify check file. Use grader.js --help for available options.");
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    } else if (program.file === undefined && program.url === undefined) {
-        console.log("Must specify a file or a url to check. Use grader.js --help for available options.");
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    } else if (program.file !== undefined && program.url !== undefined) {
-        console.log("Must specify a file or a url to check, but not both. Use grader.js --help for available options.");
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    } else
-var htmlurl = (program.file !== undefined) ? program.file : program.url;
-    checkHtmlFile(htmlurl, program.checks, true);
+      .version('1.99.1')
+      .option('-c --checks <check_file>', 'path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+      .option('-f, --file <html_file>', 'path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+      .option('-u, --url <url>', 'url to check instead of file')
+      .parse(process.argv);
+    if ( program.url && program.file == HTMLFILE_DEFAULT) {
+        var printJson = function(checkJson){
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        };
+        var checkJson = checkHtmlUrl(program.checks,printJson);
+        restler.get(program.url).on('complete', checkJson);
+    } else if ( program.url && program.file != HTMLFILE_DEFAULT) {
+        console.log("You gave me both an url and a file on disk but I can't check both on the same time.");
+        process.exit(1);
+    } else {
+        checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlUrl = checHtmlUrl;
 }
